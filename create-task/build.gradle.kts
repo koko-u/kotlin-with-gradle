3e9cdb0a33c55316e3d366a6e02066918f35eb64
Kozaki, Tsuneaki <kozaki.tsuneaki@gmail.com>
2017-06-24 22:22:45 +0900

    互いに依存するタスクを作成する

--- create-task/build.gradle.kts
+++ create-task/build.gradle.kts
@@ -1,17 +1,25 @@
-
+val taskDescription = "Projects and Tasks"
 // ProjectExtensions.kt に task 拡張関数が定義されている
 // fun Project.task(name: String, configuration: Task.() -> Unit): DefaultTask
 // DefaultTask ではなく、タスクのタイプを指定したい場合は
 // task<Copy>("helloCopy") { ... } のような風に定義できる
-task("hello") {
+val hello = task("hello") {
     description = "Simple print Hello World"
-    group = "Projects and Tasks"
-    
+    group = taskDescription
+
     doLast {
         println("Hello World!")
     }
 }
 
-task<Copy>("helloCopy") {
+val intro = task("intro") {
+    description = "DependsOn Sample Task"
+    group = taskDescription
 
+    dependsOn(hello)
+
+    doLast {
+        println("I'm Gradle")
+    }
 }
+
